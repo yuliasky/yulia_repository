ce83f74a14906f7326a5f56cb4534df78e90e54f
Yulia <julia_palomino@hotmail.com>
2019-08-11 00:59:12 -0500

    Homework Week10 SQLAlchemy

--- Homework/Week10-SQLAlchemy/app_Climate.py
+++ Homework/Week10-SQLAlchemy/app_Climate.py
@@ -41,8 +41,10 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/start_date<br/>"
+        f"/api/v1.0/start_date/end_date<br/>"
+        f"Note! start_date and end_date format: AAAA-MM-DD<br/>"
+        f"Max date: '2017-08-23'"
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -88,9 +90,8 @@ def tobs():
 
     """Return a JSON list of Temperature Observations (tobs) for the previous year"""
     # query for the dates and temperature observations from a year from the last data point
-    latest_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
 
-    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date > latest_date).\
+    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date > '2016-08-22').\
     order_by(Measurement.date).all()
 
     session.close()
@@ -99,6 +100,42 @@ def tobs():
     all_tobs = list(np.ravel(results))
 
     return jsonify(all_tobs)
+
+@app.route("/api/v1.0/<start>")
+def start(start):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start"""
+    # When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
+
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= str(start)).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    temperatures = list(np.ravel(results))
+
+    return jsonify(temperatures)
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start,end):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start"""
+    # When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
+
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= str(start)).filter(Measurement.date <= str(end)).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    temperatures = list(np.ravel(results))
+
+    return jsonify(temperatures)
 
 
 if __name__ == '__main__':
